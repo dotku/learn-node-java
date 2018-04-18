fab617129533b4a2c4c82cbbb4025ee5d2c4ff30
Weijing Jay Lin <lwjct@hotmail.com>
2018-04-18 05:51:26 +0000

    failed on main

---
+++ Hello/HelloTest.java
@@ -0,0 +1,11 @@
+package Hello;
+
+public class HelloTest {
+    public static HelloWorld hw = new HelloWorld();
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println(hw.sayHelloWorld());
+    }
+
+}
--- HelloWorldTest.js
+++ HelloWorldTest.js
@@ -1,6 +1,16 @@
 var java = require("java");
 java.classpath.push('./');
-var javaLangSystem = java.import('java.lang.System');
-javaLangSystem.out.printlnSync('Hello World from Node');
+// var javaLangSystem = java.import('java.lang.System');
+// javaLangSystem.out.printlnSync('Hello World from Node');
 
-java.newInstanceSync('HelloWorld').sayHelloWorld();
+// let helloWorld = java.newInstanceSync('Hello.HelloWorld');
+// console.log(helloWorld.sayHelloWorldSync());
+
+// let helloWorldTest = java.newInstanceSync('Hello.HelloTest');
+// console.log(helloWorldTest;
+
+let hw = java.import('Hello.HelloWorld');
+console.log(hw);
+console.log(hw.mainSync());
+// console.log(hw.main());
+// console.log(hw.sayHelloWorldSync());
